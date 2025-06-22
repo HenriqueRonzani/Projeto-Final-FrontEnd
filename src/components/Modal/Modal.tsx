75acf08c5dbfd1c75f3a4f5e8fe6d1208c56e754
Henrique <henriquedasilvaronzani@gmail.com>
2025-06-22 01:14:48 -0300

    Adicionando componente de modal e funcionalidade de excluir posts

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+};
+
+export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div
+      onClick={onClose}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-background"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="relative w-full max-w-md p-6 bg-surface rounded-lg shadow-xl"
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Post/DeleteModal.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "@/components/Form/Button";
+import {deletePost} from "@/services/postService";
+import Modal from "@/components/Modal/Modal";
+import {useRouter} from "next/navigation";
+
+interface DeletePostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  postId: number;
+}
+
+export default function DeletePostModal({isOpen, onClose, postId}: DeletePostModalProps) {
+  const router = useRouter();
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleDelete = async () => {
+    await deletePost(postId);
+    onClose();
+    router.refresh();
+  }
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className='flex flex-col gap-5 p-5 items-center bg-surface'>
+        <span>Você tem certeza que deseja excluir este post?</span>
+        <div className={'flex flex-row gap-2'}>
+          <Button className='px-5' onClick={handleDelete}>Excluir</Button>
+          <Button className='px-5' onClick={onClose}>Cancelar</Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
